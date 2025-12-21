700579f4d0e1ce5e4e397995884941abe191c8d5
Jonathan-Mamba <135890743+Jonathan-Mamba@users.noreply.github.com>
2025-12-21 15:15:21 +0100

    Added Tache.vue

---
+++ src/Tache.vue
@@ -0,0 +1,26 @@
+<template>
+  <label :class="{ barred: completed}" >
+    <input type="checkbox" v-model="model" @change="onChange">
+    <slot></slot>
+  </label>
+</template>
+
+<script setup>
+  const model = defineModel()
+  const props = defineProps({
+    completed: Boolean,
+    date: Number
+  })
+  const emits = defineEmits(["check", "uncheck"])
+  const onChange = (event) => {
+    if (event.currentTarget.checked) { emits("check") }
+    else { emits("uncheck") }
+  }
+</script>
+
+<style scoped>
+  .barred {
+    text-decoration-style: dashed;
+    background-color: #555;
+  }
+</style > 
