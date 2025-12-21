4935d41ab74955a09e425e2b96633bdec0f213a0
Jonathan-Mamba <135890743+Jonathan-Mamba@users.noreply.github.com>
2025-12-21 14:02:15 +0100

    Todo list correction

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <h1>Todo list</h1>
-  <p>{{  }}</p>
   <form @submit.prevent="ajouterTache">
     <input type="text" placeholder="Name" v-model="nomTache">
     <button type="submit">Ajouter la tache</button>
@@ -13,58 +12,39 @@
     <label for="checkbox-masquer">Masquer les tâches complétées.</label>
     <p>Tâches:</p>
 
-    <div v-for="[index, name] in listeTaches.entries()">
-      <input type="checkbox" :name="index" v-model="listeCoches" :value="index" v-if="!(masquerTerm && verifierCoche(index))" @change="trierTaches()">
-      <label :for="index" :class="{barred: verifierCoche(index)}" v-if="!(masquerTerm && verifierCoche(index))">{{name}}</label>
+    <div v-for="tache in getListeTaches">
+      <label :class="{barred: tache.completed}" :key="tache.date">
+        <input type="checkbox" v-model="tache.completed">
+        {{tache.title}}
+      </label>
     </div>
   </div>
 </template>
 
 
 <script setup>
-  import {ref} from 'vue';
+  import {computed, ref} from 'vue';
 
   const listeTaches = ref([]);
   const nomTache = ref('');
-  const listeCoches = ref([]);
   const masquerTerm = ref(false);
 
-  const verifierCoche = (index) => {
-    for (const tache of listeCoches.value) {
-      if (index === tache) {
-        return true;
-      }
+  const getListeTaches = computed(() => {
+    let l = listeTaches.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+    if (masquerTerm.value) {
+      return l.filter((tache) => !tache.completed)
     }
-    return false;
-  }
+    return l
 
-  const compareTrucs = (a, b) => {
-    if (a[1] === b[1]) {
-       return 0;
-    } else if (a[1]) {
-      return 1
-    } else {
-      return -1;
-    }
-  }
-  const trierTaches = () => {
-    const arrayCoche = Array(listeTaches.value.length);
-    arrayCoche.fill(false);
-    for (const index of listeCoches.value) {
-      arrayCoche[index] = true;
-    }
-    const zipped = listeTaches.value.map((value, index) => [value, arrayCoche[index]]);
-    console.log(zipped);
-    zipped.sort(compareTrucs);
-    console.log(zipped);
-    listeTaches.value = zipped.map((value) => value[0]);
-    listeCoches.value = zipped.map((value, index) => value[1] ? index : -1).filter((value) => value >= 0);
-  }
+  })
 
   const ajouterTache = () => {
-    listeTaches.value.push(nomTache.value);
+    listeTaches.value.push({
+      title: nomTache.value,
+      completed: false,
+      date: Date.now(),
+    });
     nomTache.value = '';
-    trierTaches();
   };
 </script>
 
