da47ba09acc52f3ab7858805a7f94ad6ac0f41b4
Jonathan Mamba DIALLO <jonathan.mamba.diallo@gmail.com>
2026-02-21 15:04:08 +0100

    Moved Vector2 and DrawnRoute classes

--- src/components/map/GameMap.vue
+++ src/components/map/GameMap.vue
@@ -1,8 +1,76 @@
 <script setup lang="ts">
-  import {squares, Vector2, DrawnRoute, routes, getRoutes, Route} from "@/util";
+  import {squares, routes, getRoutes, Route, routeTypes} from "@/util";
   import {Ref, ref, computed} from "vue";
   import { useElementSize } from "@vueuse/core";
   getRoutes();
+
+  export class Vector2 {
+    x: number;
+    y: number;
+    constructor(x: number = 0, y: number = 0) {
+      this.x = x;
+      this.y = y;
+    }
+    public add(other: Vector2): Vector2 {
+      return new Vector2(this.x + other.x, this.y + other.y);
+    } 
+    public sub(other: Vector2): Vector2 {
+      return new Vector2(this.x - other.x, this.y - other.y)
+    }
+    public toString(): string {
+      return `(${this.x}, ${this.y})`;
+    }
+    public rotate(angle: number): Vector2 {
+      const cos = Math.cos(angle);
+      const sin = Math.sin(angle);
+      return new Vector2(
+        this.x * cos - this.y * sin,
+        this.x * sin + this.y * cos
+      );
+    }
+  }
+
+  class DrawnRoute {
+    firstEnd: Vector2;
+    secondEnd: Vector2;
+    color: string;
+    curved: boolean = false;
+    circleCenter: Vector2 = new Vector2();
+    circleRadius: number = 0;
+    constructor(first_end: Vector2, second_end: Vector2, color: string, curved: boolean = false) {
+      this.firstEnd = first_end;
+      this.secondEnd = second_end;
+      this.color = color;
+      this.curved = curved;
+    }
+    public static fromRoute(route: Route, squarePositions: Vector2[]): DrawnRoute {
+      const first_end = squarePositions[squares.value.indexOf(route.firstEnd)];
+      const second_end = squarePositions[squares.value.indexOf(route.secondEnd)];
+      const color = routeTypes.value[route.type];
+      const curved = route.firstEnd === route.secondEnd
+      return new DrawnRoute(first_end, second_end, color, curved);
+    }
+    public getCircleCenter(mapCenter: Vector2, circleRadius: number, squareSize: Vector2): Vector2 {
+      if (!this.curved) {
+        throw Error("route is not curved");
+      }
+      const dx = this.firstEnd.x - mapCenter.x;
+      const dy = this.firstEnd.y - mapCenter.y;
+      const circleVector = new Vector2();
+      if (dx > 0 && dx > dy) { // right
+        circleVector.x = squareSize.x / 2 + circleRadius 
+      } else if (dx < 0 && dx < dy) { // left
+        circleVector.x = -squareSize.x / 2 - circleRadius
+      } else if (dy < 0 && dy < dx) { // up
+        circleVector.y = -squareSize.y / 2 - circleRadius
+      } else { // down
+        circleVector.y = squareSize.y / 2 + circleRadius
+      }
+      this.circleCenter = this.firstEnd.add(circleVector)
+      this.circleRadius = circleRadius
+      return this.firstEnd.add(circleVector)
+    }
+  }
 
   const mapSVG: Ref<SVGSVGElement | null> = ref(null);
   const { width, height } = useElementSize(mapSVG);
@@ -12,7 +80,7 @@
     return new Vector2(0.25 * width.value,  0.12 * height.value)
   });
   const center = computed(() => (new Vector2(width.value / 2, height.value / 2)))
-  const circleRadius = ref(20)
+
 
   const squarePositions: Ref<Vector2[]> = computed(() => {
     const result: Vector2[] = [];
@@ -29,6 +97,7 @@
     const result: DrawnRoute[] = [];
     const routeMap: Map<string, Route[]> = new Map();
     const lineOffset = 2 * lineWidth.value;
+    const circleRadius = 20;
     let diff_vector: Vector2;
 
     for (const route of routes.value) {
@@ -43,9 +112,9 @@
         const drawnRoute = DrawnRoute.fromRoute(routeGroup[i], squarePositions.value);
         diff_vector = drawnRoute.firstEnd.sub(drawnRoute.secondEnd)
         if (drawnRoute.curved) {
-          drawnRoute.getCircleCenter(center.value, circleRadius.value, squareSize.value)
-          drawnRoute.circleRadius = circleRadius.value + lineOffset * i
-        }else if (-1 < diff_vector.y / diff_vector.x && diff_vector.y / diff_vector.x < 1) {
+          drawnRoute.getCircleCenter(center.value, circleRadius, squareSize.value)
+          drawnRoute.circleRadius = circleRadius + lineOffset * i
+        } else if (-1 < diff_vector.y / diff_vector.x && diff_vector.y / diff_vector.x < 1) {
           drawnRoute.firstEnd = drawnRoute.firstEnd.add(new Vector2(0, lineOffset * i))
           drawnRoute.secondEnd = drawnRoute.secondEnd.add(new Vector2(0, lineOffset * i))
         } else {
@@ -64,10 +133,10 @@
     <svg ref="mapSVG">
       <g v-for="route in drawnRoutes">
         <line v-if="!route.curved" :x1="route.firstEnd.x" :y1="route.firstEnd.y" :x2="route.secondEnd.x" :y2="route.secondEnd.y" :stroke="route.color" :stroke-width="lineWidth"/>
-        <circle v-else :cx="route.circleCenter.x" :cy="route.circleCenter.y" :r="route.circleRadius" :stroke="route.color" :stroke-width="lineWidth" fill="none"></circle>
+        <circle v-else :cx="route.circleCenter.x" :cy="route.circleCenter.y" :r="route.circleRadius" :stroke="route.color" :stroke-width="lineWidth" fill="none"/>
       </g>
       <g v-for="[index, square] in squares.entries()">
-        <rect :x="squarePositions[index].x - squareSize.x / 2" :y="squarePositions[index].y - squareSize.y / 2" :width="squareSize.x" :height="squareSize.y"></rect>
+        <rect :x="squarePositions[index].x - squareSize.x / 2" :y="squarePositions[index].y - squareSize.y / 2" :width="squareSize.x" :height="squareSize.y"/>
         <text :x="squarePositions[index].x" :y="squarePositions[index].y">{{ square }}</text>
       </g>
     </svg>
