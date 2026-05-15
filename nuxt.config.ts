a423ecbd4f4e186d7edfcc28afe84765dab5dd27
Jonathan Mamba DIALLO <jonathan.mamba.diallo@gmail.com>
2026-05-15 11:58:58 +0200

    fixed deploy settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/style.css'],
+  css: ['~/assets/m.css', '~/assets/style.css'],
   modules: ['@nuxt/ui'],
   ssr: false,
   icon: {
